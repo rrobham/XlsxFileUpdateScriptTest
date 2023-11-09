0f8233dd62959b2e2b38555b0413ec44cb9098eb
Robham Robham <RRobham@Paylocity.com>
2023-11-09 12:36:43 -0600

    test file read from server

--- cypress/tests/test.cy.ts
+++ cypress/tests/test.cy.ts
@@ -4,13 +4,13 @@ describe('update excel cell value', () => {
     it('read data from excel', () => {      
         console.log(`${Cypress.env('ftpHost')} ${Cypress.env('ftpUser')} ${Cypress.env('ftpPassword')}`)
         cy.log(`${Cypress.env('ftpHost')} ${Cypress.env('ftpUser')} ${Cypress.env('ftpPassword')}`)
-       
-        //cy.getFiles({host: Cypress.env('ftpHost'), user: Cypress.env('ftpUser'), password: Cypress.env('ftpPassword'), directoryPath: "/workengineautomationtesting/drprod/cy"}).then((files) => {
-        //    cy.log(files.length)
-        //    files.forEach(file => {
-        //       cy.log(file.name)
-        //    })
-        //});
+      
+        cy.getFiles({host: Cypress.env('ftpHost'), user: Cypress.env('ftpUser'), password: Cypress.env('ftpPassword'), directoryPath: "/workengineautomationtesting/drprod/cy"}).then((files) => {
+            cy.log(files.length)
+            files.forEach(file => {
+               cy.log(file.name)
+            })
+        });
     })
 
 })
